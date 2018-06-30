9ddeadeb1e8367a7fd7360f6e9052009375e2dc9
Mohamed Fareed <mfareed2014@gmail.com>
2018-06-30 21:43:40 +0200

    importing dagger component

--- app/src/main/java/app/elmenus/ElMenusApp.java
+++ app/src/main/java/app/elmenus/ElMenusApp.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import app.elmenus.presentation.injection.AppComponent;
 import app.elmenus.presentation.injection.AppModule;
+import app.elmenus.presentation.injection.DaggerAppComponent;
 import app.elmenus.presentation.injection.NetworkModule;
 
 public class ElMenusApp extends Application {
