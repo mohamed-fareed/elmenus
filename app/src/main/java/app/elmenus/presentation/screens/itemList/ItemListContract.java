a38aecb716925dfd18edce384c926fe2e3378212
Mohamed Fareed <mfareed2014@gmail.com>
2018-07-01 17:36:02 +0200

    finished item list screen contract

--- app/src/main/java/app/elmenus/presentation/screens/itemList/ItemListContract.java
+++ app/src/main/java/app/elmenus/presentation/screens/itemList/ItemListContract.java
@@ -1,13 +1,52 @@
 package app.elmenus.presentation.screens.itemList;
 
+import java.util.List;
+
+import app.elmenus.data.models.Item;
 import app.elmenus.presentation.base.BaseContract;
 
 public interface ItemListContract {
     interface View extends BaseContract.View {
+        void initializeView();
 
+        void initializeListAdapter();
+
+        void showItemListLoading();
+
+        void hideItemListLoading();
+
+        void showListErrorLoading();
+
+        void hideListErrorLoading();
+
+        void showSwipeToRefreshLoading();
+
+        void hideSwipeToRefreshLoading();
+
+        void addItems(List<Item> items);
+
+        void setItems(List<Item> items);
+
+        void resetList();
+
+        void setNotLoading();
+
+        void incrementAdapterPage();
+
+        void goToSingleItem(long itemId);
     }
 
     interface Presenter extends BaseContract.Presenter<BaseContract.View> {
+        void start();
 
+        void onLoadMoreTriggered(int page);
+
+        void onRefresh();
+
+        void onItemClicked(long itemId);
+
+        void onRetryFromErrorLoading(int currentPage);
+
+        void onCommentBodyChanged(CharSequence commentBody);
     }
 }
