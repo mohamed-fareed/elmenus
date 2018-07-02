ff7e299a5228b829fe8844adbdf9a694bc9e40c4
Mohamed Fareed <mfareed2014@gmail.com>
2018-07-02 17:44:04 +0200

    applying transition between item list and list details

--- app/src/main/java/app/elmenus/presentation/utils/NavigationManager.java
+++ app/src/main/java/app/elmenus/presentation/utils/NavigationManager.java
@@ -1,6 +1,8 @@
 package app.elmenus.presentation.utils;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v4.app.Fragment;
 
 import app.elmenus.presentation.screens.singleItem.SingleItemActivity;
@@ -9,6 +11,11 @@ public class NavigationManager {
     public static void goToSingleItem(Fragment fragment, long itemId) {
         Intent intent = new Intent(fragment.getActivity(), SingleItemActivity.class);
         intent.putExtra(Constants.ITEM_ID, itemId);
-        fragment.startActivity(intent);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            fragment.startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(fragment.getActivity()).toBundle());
+        } else {
+            fragment.startActivity(intent);
+        }
     }
 }
